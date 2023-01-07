71d13ca8cc9c90c1b96f0a3adf87f55c16e8198d
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-01-08 00:56:39 +0100

    create service to fetch projects from API, add CORs whitelist to Django, display test data on portfolio page to check data was transfered to the client

--- api/portfolioApi/settings.py
+++ api/portfolioApi/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary',
 
     'rest_framework',
+    'corsheaders',
 
     'projectsApi',
 ]
@@ -52,12 +53,18 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:4200',
+)
 
 ROOT_URLCONF = 'portfolioApi.urls'
 
