9eb168dc543c3e8bc5c8d85d6c614e1836679a97
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-05 23:46:25 +0100

    amend project api endpoint to only accept get requests

--- api/projectsApi/views.py
+++ api/projectsApi/views.py
@@ -1,5 +1,6 @@
 
-from rest_framework import viewsets
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin
 
 from .serializers import ProjectSerializer
 from .models import Project
@@ -7,11 +8,11 @@ from .models import Project
 # Create your views here.
 
 
-class ProjectViewSet(viewsets.ModelViewSet):
+class ProjectViewSet(GenericViewSet, ListModelMixin):
     queryset = Project.objects.filter(status=1).order_by("completion_date")
     serializer_class = ProjectSerializer
 
 
-class UpcomingProjectViewSet(viewsets.ModelViewSet):
+class UpcomingProjectViewSet(GenericViewSet, ListModelMixin):
     queryset = Project.objects.filter(status=2).order_by("completion_date")
     serializer_class = ProjectSerializer
