e01aa1c02c65daf1d6ad4b86656a7c33099e2ad5
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-06 23:07:21 +0100

    add contact request to api admin panel

--- api/contactRequestsApi/admin.py
+++ api/contactRequestsApi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ContactRequest
 
 # Register your models here.
+@admin.register(ContactRequest)
+class contactRequestsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'reason')
