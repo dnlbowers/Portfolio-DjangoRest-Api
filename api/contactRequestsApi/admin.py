1a989fdcb64a20f5df86e7e4a0c286f3b99cfd6d
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-10 15:02:35 +0100

    connect emails up to contact me directly when contact request is created in the db

--- api/contactRequestsApi/admin.py
+++ api/contactRequestsApi/admin.py
@@ -4,4 +4,4 @@ from .models import ContactRequest
 # Register your models here.
 @admin.register(ContactRequest)
 class contactRequestsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'reason')
+    list_display = ('name', 'email', 'reason', 'created_at')
--- api/contactRequestsApi/models.py
+++ api/contactRequestsApi/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
 
 # Create your models here.
 class ContactRequest(models.Model):
@@ -6,6 +12,30 @@ class ContactRequest(models.Model):
     email = models.EmailField()
     reason = models.CharField(max_length=30)
     message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+    
+    def _send_as_email(self):
+        """
+        Send request as email
+        """
+        customer_email = self.email
+        subject = render_to_string(
+            'contactRequestsApi/email/email_subject.txt',
+            {'contact': self})
+        body = render_to_string(
+            'contactRequestsApi/email/email_body.txt',
+            {'contact': self})
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [customer_email]
+        )
+        
+@receiver(post_save, sender=ContactRequest)
+def _post_save_receiver(sender, instance, **kwargs):
+    sender._send_as_email(instance)
