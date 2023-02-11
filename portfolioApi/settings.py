b8e8079f1714859fae844ac6f4008b68cc7a8c45
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-11 23:41:33 +0100

    amend static settings

--- portfolioApi/settings.py
+++ portfolioApi/settings.py
@@ -20,7 +20,6 @@ if os.path.isfile('portfolioApi/.env'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -141,7 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CLOUDINARY = {
     'CLOUD_NAME': os.environ.get("CLOUDINARY_CLOUD_NAME"),
