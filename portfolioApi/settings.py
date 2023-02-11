afdaf6949d3cffd21bef4fabc31399266bbeb7ec
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-11 20:55:27 +0100

    add static handler to settings.py

--- portfolioApi/settings.py
+++ portfolioApi/settings.py
@@ -20,6 +20,7 @@ if os.path.isfile('portfolioApi/.env'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -140,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 CLOUDINARY = {
     'CLOUD_NAME': os.environ.get("CLOUDINARY_CLOUD_NAME"),
