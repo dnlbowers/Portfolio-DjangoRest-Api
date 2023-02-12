d50ea93ee82b3477abff4e4c6cb4629e81e12a6f
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-12 22:59:13 +0100

    add cloudinary storage

--- portfolioApi/settings.py
+++ portfolioApi/settings.py
@@ -148,7 +148,7 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
-CLOUDINARY = {
+CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get("CLOUDINARY_CLOUD_NAME"),
     'API_KEY': os.environ.get("CLOUDINARY_API_KEY"),
     'API_SECRET': os.environ.get("CLOUDINARY_API_SECRET"),
