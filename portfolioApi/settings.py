5a268fc7962ed6ca013e72aa4ccdc5b44ca0229a
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-13 23:32:34 +0100

    amend static root and urls

--- portfolioApi/settings.py
+++ portfolioApi/settings.py
@@ -141,10 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 
--- portfolioApi/urls.py
+++ portfolioApi/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/portfolio/", include("projectsApi.urls", namespace="projectsApi")),
     path("api/contact/", include("contactRequestsApi.urls", namespace="contactRequestsApi")),
-] + static(settings.STATIC_URL, settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
