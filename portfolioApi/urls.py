ad2fa1a6ffd6179f388fc8742edca22154601371
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-13 23:13:29 +0100

    move static folder into git repo

--- portfolioApi/urls.py
+++ portfolioApi/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/portfolio/", include("projectsApi.urls", namespace="projectsApi")),
     path("api/contact/", include("contactRequestsApi.urls", namespace="contactRequestsApi")),
-]
+] + static(settings.STATIC_URL, settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
