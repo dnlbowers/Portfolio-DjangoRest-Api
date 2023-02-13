43df88b81b9fd8571edc893bf70325a4eb168de0
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-14 00:50:32 +0100

    tweak settings for aws

--- portfolioApi/settings.py
+++ portfolioApi/settings.py
@@ -20,7 +20,7 @@ if os.path.isfile('portfolioApi/.env'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -42,16 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary',
+    'projectsApi',
+    'contactRequestsApi',
 
     'rest_framework',
+    'cloudinary',
     'corsheaders',
     'storages',
-
-    'projectsApi',
-    'contactRequestsApi'
 ]
 
 MIDDLEWARE = [
@@ -75,7 +73,9 @@ ROOT_URLCONF = 'portfolioApi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATE_DIR, os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -142,7 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
