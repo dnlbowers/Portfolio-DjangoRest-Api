5572ff4066da73fe315c1f91c88830509c930236
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-13 01:52:41 +0100

    allowed host subdomains

--- portfolioApi/settings.py
+++ portfolioApi/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.dev-bowers.com.',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'djangoapi.dev-bowers.com',]
 
 
 # Application definition
