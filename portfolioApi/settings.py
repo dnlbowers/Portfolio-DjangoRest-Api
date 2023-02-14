e704c1cf39dc5ed30888e9d452f9533af53f0742
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-14 01:12:05 +0100

    tweak settings for aws

--- portfolioApi/settings.py
+++ portfolioApi/settings.py
@@ -142,7 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
