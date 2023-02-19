906ed720e914b8db897e18de527403e04fe3cf7f
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-02-19 11:53:06 +0100

    remove trailing /

--- portfolioApi/settings.py
+++ portfolioApi/settings.py
@@ -67,7 +67,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:4200',
     'https://portfoliosite.dev-bowers.com',
-    'https://dev-bowers.com/'
+    'https://dev-bowers.com'
 )
 
 ROOT_URLCONF = 'portfolioApi.urls'
